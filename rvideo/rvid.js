1fb017c76f380a7c966d47c548fd7aff1bba6b9b
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-05-24 11:22:27 +0530

    Update rvid.js

--- rvideo/rvid.js
+++ rvideo/rvid.js
@@ -8,12 +8,12 @@ if (Hls.isSupported()) {
   // bind them together
   hls.attachMedia(video);
   hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-    console.log('Apna kaam kar lawde');
+    //console.log('Apna kaam kar lawde');
     hls.loadSource(c2);
     hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-      console.log(
-        'Apna kaam kar lawde'
-      );
+      //console.log(
+      //  'Apna kaam kar lawde'
+      //);
     });
   });
 }

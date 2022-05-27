2607c3314af3a1be1635bbca7c68234a8b45a5e6
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-05-27 16:36:14 +0530

    Create script.js

---
+++ rvideo2/script.js
@@ -0,0 +1,21 @@
+var url_string = window.location.href; //window.location.href
+var url = new URL(url_string);
+var c = ((url.searchParams.get("url"))?url.searchParams.get("url"):"vf4k6vipfmo81");
+
+var options = {
+  controls: true,
+  preload: 'metadata',
+  liveui: true,
+  playbackRates: [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0],
+  controlBar: {
+    children: ['playToggle', 'volumePanel', 'currentTimeDisplay', 'timeDivider', 'durationDisplay', 'progressControl', 'remainingTimeDisplay', 'Spacer', 'captionsButton', 'qualitySelector', 'playbackRateMenuButton', 'fullscreenToggle']
+  },
+  plugins: {
+          httpSourceSelector: {
+            displayCurrentQuality: true
+          }
+        }
+};
+
+var player = videojs("video_player", options);
+player.src("https://v.redd.it/"+c+"/HLSPlaylist.m3u8");

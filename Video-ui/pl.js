91d882b6ed6015dec464318c51bdcc909addde02
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-06-01 14:03:31 +0530

    Update pl.js

--- Video-ui/pl.js
+++ Video-ui/pl.js
@@ -1,4 +1,6 @@
 var url = new URL(window.location.href);
-document.getElementById("ideo").src = "https://drive.google.com/uc?id=" + c + url.searchParams.get("url");
+console.log(url)
+console.log("https://drive.google.com/uc?id=" + url.searchParams.get("url"))
+document.getElementById("ideo").src = "https://drive.google.com/uc?id=" + url.searchParams.get("url");
 document.getElementById("my-video").poster = "https://drive.google.com/thumbnail?id=" + url.searchParams.get("url");
 document.getElementById("my-video").preload = url.searchParams.get("loading");
